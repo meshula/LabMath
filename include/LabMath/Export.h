549a383efdb0e3008585e66b656507f99179ffd5
Nick Porcino <meshula@hotmail.com>
2021-08-08 20:48:00 -0700

    better shared/static handling

--- include/LabMath/Export.h
+++ include/LabMath/Export.h
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#if defined(_MSC_VER) && !defined(LABMATH_STATIC)
+#if defined(_MSC_VER) && defined(LABMATH_SHARED)
 # ifdef BUILDING_LabMath
 #  define LM_CAPI extern "C" __declspec(dllexport)
 #  define LM_API __declspec(dllexport)
